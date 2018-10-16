02ae8a76592e6ea27b24abe91707e9fbc2d1d058
kbaynes <kevin1421@baynes.net>
2018-10-16 08:25:56 -0400

    More info in readme. Converted app base to a class.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Express-Typescript
-Example Web Server Project using Express 4 and Typescript 2.9
+# Express-Typescript Quickstart
+
+Example Basic Web Server Project using Express 4 and Typescript 2.9
+
+This application shows how to create and organize a basic web app with Express 4 and Typescript 2.9. It is not intended to be a complete platform. All files are TypeScript classes which transpile to JavaScript (ECMAScript 2016) and then executed. Requires Node 8+.
+
+## To Run
+Clone this repository
+Open a command line to the folder
+Run 'npm install'
+Run 'npm run serve'
+Open a browser to 'http://localhost:3000/'
--- app-ts/app.ts
+++ app-ts/app.ts
@@ -1,20 +1,31 @@
 import express from 'express';
-
 // import controllers from module - add new controllers to module
 import { TestController, WelcomeController } from './controllers/controllers.module';
 
-// Create a new express application instance
-const app: express.Application = express();
+export class Application {
+    
+    // Create a new express application instance
+    express: express.Application = express();
+    
+    // The port the express server listens
+    port: number = !!process.env.PORT ? +process.env.PORT : 3000;
 
-// The port the express server listens
-const port: number = !!process.env.PORT ? +process.env.PORT : 3000;
+    constructor() {
 
-// setup route handling
-app.use('/', new WelcomeController().router);
-app.use('/test', new TestController().router);
+        // setup route handling
+        this.express.use('/', new WelcomeController().router);
+        this.express.use('/test', new TestController().router);
+    }
+    
+    run() {
+        
+        // Serve the application
+       this.express.listen(this.port, () => {
+            // Success callback
+            console.log(`Listening at http://localhost:${this.port}/`);
+        });
+    }
+}
 
-// Serve the application
-app.listen(port, () => {
-    // Success callback
-    console.log(`Listening at http://localhost:${port}/`);
-});
+const app: Application = new Application();
+app.run();
